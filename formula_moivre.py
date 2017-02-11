62884e88f79c4eda7bb41ae4772a499bab43dc38
doc_aran <doctorarang@gmail.com>
2017-02-11 00:24:42 -0600

    Formula de Moivre funcionando

---
+++ formula_moivre.py
@@ -0,0 +1,29 @@
+import cmath
+import math
+
+def sacar_raices(c, n):
+    """
+    sacar_raices(complejo, indice) -> [raiz_1, raiz_2, ... , raiz_n]
+
+    Devuelve todas las raíces de un radicando complejo con indice n
+
+    Argumentos:
+    c: radicando complejo
+    n: indice de la raíz
+    """
+    
+    plr = cmath.polar(c)
+    
+    angulo = plr[1]
+    if angulo < 0:
+        angulo += math.pi * 2
+
+    raices = []
+    raiz_radio = plr[0] ** (1. / n)
+
+    for i in range(n):
+        i_theta = (angulo + 2 * math.pi * i) / n
+        raices.append([raiz_radio, math.degrees(i_theta)])
+
+    return raices
+#Fin función sacar_raices
